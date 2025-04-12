c98b01db6d5e7f6528aa0ddb647e4ce540b0ae5b
Paweł Woźny <pawel.wozny@gmail.com>
2025-04-12 11:58:06 +0200

    Initial commit

---
+++ src/main/java/com/sportradar/footballscoreboard/FootballscoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.sportradar.footballscoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballscoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballscoreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportradar/footballscoreboard/FootballscoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportradar.footballscoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballscoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
